6c264da2c24882b4557fd0510eba705b308ada15
Ivan <ivanmikhailenka1@gmail.com>
2018-09-22 21:33:45 +0300

    implementation

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+    let countOfTriangles = 0;
+    let countOfLinks = 0;
+    for (const [i, value] of preferences.entries()) {
+        let secondValue = preferences[value - 1];
+        let thirdValue = preferences[secondValue - 1];
+        if (i === (thirdValue - 1)) {
+            countOfLinks++;
+        }
+        if(countOfLinks === 3){
+            countOfTriangles++;
+            countOfLinks = 0;
+        }
+    }
+    return countOfTriangles;
 };
